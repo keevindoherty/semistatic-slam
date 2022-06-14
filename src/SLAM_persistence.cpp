448d2659f46c0523e3c859826cd729cfd2dcf2b0
ishicode <goluguriishita@gmail.com>
2022-06-14 16:50:06 -0400

    Progress

--- src/SLAM_persistence.cpp
+++ src/SLAM_persistence.cpp
@@ -16,28 +16,31 @@
 
 
 
-//Consider robot in a 5 by 5 grid
-//Landmark coordinates at (2,0), (4,3), (3,5)
-//Robot moves 1 space over and one space up in each step
-//Assume the robot does 5 passes through the grid
-//On pass 1, landmark one moves to (2,1)
-//On pass 2, landmark two moves to (3,2)
-//One pass 4, landmark three disappears
+/*To change the positions or ground_truth_landmarks:
+    - Change ground_truth_num_landmarks
+    - Change num_poses
+    - Changed ground_truth_poses
+    - Change ground_truth_landmarks
+    - Change initial_estimate_psoes
+    - Change initial_estimate_landmarks
+*/
 
 using namespace std;
 using namespace gtsam;
 
+bool debug = false;
+
 const int ground_truth_num_landmarks = 3;
 const int max_num_landmarks = 6;
-const int num_poses = 6;
+const int num_poses = 7;
 
-static Point3 ground_truth_poses[num_poses] = {Vector3(0.0, 0.0, 0.0), Vector3(1.0, 1.0, 0.0), Vector3(2.0, 2.0, 0.0), Vector3(3.0, 3.0, 0.0), Vector3(4.0, 4.0, 0.0), Vector3(5.0, 5.0, 0.0)};
+static Point3 ground_truth_poses[num_poses] = {Vector3(0.0, 0.0, 0.0), Vector3(1.0, 1.0, 0.0), Vector3(2.0, 2.0, 0.0), Vector3(3.0, 3.0, 0.0), Vector3(4.0, 4.0, 0.0), Vector3(5.0, 5.0, 0.0), Vector3(0.0,0.0,0.0)};
 
-Point3 ground_truth_landmarks[ground_truth_num_landmarks] = {Vector3(2.0,0.0,0.0), Vector3(2.0,3.0,0.0), Vector3(3.0,5.0,0.0)};
+Point3 ground_truth_landmarks[ground_truth_num_landmarks] = {Vector3(2.0,1.0,0.0), Vector3(2.0,3.0,0.0), Vector3(3.0,5.0,0.0)};
 
-Pose2 initial_estimate_poses[6] = {Pose2(3.07, 2.49, 0.73), Pose2(1.58, 4.3, 2.72), Pose2(0.44, 3.78, -2.05319), Pose2(2.09, 4.4, 1.65), Pose2(4.92, 0.42, -1.41319), Pose2(0.03, 3.27, 2.29)};
+Pose2 initial_estimate_poses[num_poses] = {Pose2(3.07, 2.49, 0.73), Pose2(1.58, 4.3, 2.72), Pose2(0.44, 3.78, -2.05319), Pose2(2.09, 4.4, 1.65), Pose2(4.92, 0.42, -1.41319), Pose2(0.03, 3.27, 2.29), Pose2(2.09, 4.4, 1.65)};
   
-Point2 initial_estimate_landmarks[3] = {Point2(3.4, 1.12), Point2(3.03, 1.69), Point2(2.09, 1.57)};
+Point2 initial_estimate_landmarks[ground_truth_num_landmarks] = {Point2(3.4, 1.12), Point2(3.03, 1.69), Point2(2.09, 1.57)};
 
 NonlinearFactorGraph graph;
 
@@ -45,7 +48,7 @@ static Vector3 odometry(1.0, 1.0, 0.0);
 
 static Vector2 mNoise(0.01, 0.02);
 
-static Vector3 oNoise(0.02, 0.02, 0.01);
+static Vector3 oNoise(0.2, 0.2, 0.1);
 
 static double lambda_u = 1;
 static double lambda_l = .01;
@@ -58,6 +61,8 @@ static double P_F = 0.01;
 double t = 1.0;
 
 int passes = 0;
+
+Values initialEstimate2;
 
 std::uniform_real_distribution<double> distribution(0.0, 5.0);
 static std::default_random_engine generator(time(0));
@@ -98,8 +103,12 @@ double distance(Vector3 v1, Vector3 v2){
 int feature_to_index(Vector3 detected_feature){
   for (auto element: index_to_feature_map){
      if (distance(element.second, detected_feature)<0.1){
+       fprintf(stderr, "Vector: (%f, %f, %f) detected\n", detected_feature.x(), detected_feature.y(), detected_feature.z());
        return element.first;
      }
+  }
+  if(debug){
+    fprintf(stderr, "Adding new feature\n");
   }
   index_to_feature_map[current_landmark_number]= detected_feature;
   current_landmark_number++;
@@ -118,6 +127,9 @@ bool ground_truth(Vector3 v1){
   */
   for (int i =0; i < ground_truth_num_landmarks; i++){
     if (distance(ground_truth_landmarks[i], v1)<0.1){
+      if(debug){
+        fprintf(stderr, "Vector: (%f, %f, %f), in ground truth\n", v1.x(), v1.y(), v1.z());
+      }
       return true;
     }
   }
@@ -151,28 +163,20 @@ bool persistence(int landmark_no){
   }
   else{
     landmark_filters[landmark_no].update(false, t, P_M, P_F);
+    if(debug){
+      fprintf(stderr, "landmark no = %d. This is a fake landmark\n", landmark_no);
+    }
   }
   if (landmark_filters[landmark_no].predict(t) > 0.75){
-    //return true;
+    return true;
   }
-  return true;
+  return false;
 }
 
-
-// void update_graph_landmark(NonlinearFactorGraph graph, int i, Vector3 pose, Vector3 landmark){
-//   auto measurementNoise = noiseModel::Diagonal::Sigmas(
-//       mNoise);
-//   /* Adds landmark to the graph */
-//   double angle = atan((landmark.y()-pose.y())/(landmark.x()-pose.x()));
-//   Rot2 bearing = Rot2::fromAngle(add_noise(mNoise.y(), angle));
-//   bearing.print();
-//   double range = sqrt(pow(landmark.y()-pose.y(), 2.0)+pow(landmark.x()-pose.x(), 2.0));
-//   double noisy_range = add_noise(mNoise.x(), range);
-//   graph.emplace_shared <BearingRangeFactor<Pose2, Point2>> (poses[i], landmarks[feature_to_index(landmark)], bearing, noisy_range, measurementNoise);
-// }
-
-
-void one_pass(NonlinearFactorGraph graph) {
+void one_pass() {
+  if(debug){
+  fprintf(stderr, "Pass %d: \n", passes);
+  }
   Vector3 pNoise(0.0,0.0,0.0);
     auto priorNoise = noiseModel::Diagonal::Sigmas(
       pNoise);
@@ -188,12 +192,15 @@ void one_pass(NonlinearFactorGraph graph) {
   }
   for (int i = 0; i < num_poses-1; i++){
     //Add a factor between the two poses
+    Vector3 odometry(ground_truth_poses[i+1].x()-ground_truth_poses[i].x(), ground_truth_poses[i+1].y()-ground_truth_poses[i].y(), ground_truth_poses[i+1].z()-ground_truth_poses[i].z());
     Pose2 noisy_odometry = Pose2(add_noise(oNoise.x(), odometry.x()), add_noise(oNoise.y(), odometry.y()), add_noise(oNoise.z(), odometry.z()));
     graph.emplace_shared<BetweenFactor<Pose2> >(Symbol('x',current_pose_index), Symbol('x',current_pose_index+1), noisy_odometry, odometryNoise);
     int map_size = index_to_feature_map.size();
     for (int j = 0; j <map_size; j++){
       bool p = persistence(j);
-      //fprintf(stderr, "p: %d, i: %d,j: %d\n", p, i, j);
+      if(debug){
+         fprintf(stderr, "p = %d, i = %d, j = %d\n", p, i, j);
+      }
       Vector3 feat = index_to_feature(j);
       if (feat.x() >= ground_truth_poses[i].x() && p){
         double angle = atan((feat.y()-ground_truth_poses[i].y())/(feat.x()-ground_truth_poses[i].x()));
@@ -209,29 +216,33 @@ void one_pass(NonlinearFactorGraph graph) {
     t+=1.0;
   }
   Values initialEstimate;
-  Values initialEstimate2;
-  
-  for (int i = 0; i < num_poses; i++){
+  fprintf(stderr, "Before initial estimate\n");
+  if(passes ==0){
+    initialEstimate2.insert(Symbol('x',0), initial_estimate_poses[0]);
+  for (int i = 0; i < ground_truth_num_landmarks; i++){
+    initialEstimate2.insert(Symbol('l',i), initial_estimate_landmarks[i]);
+   }
+  }
+  for (int i = 1; i < num_poses; i++){
       initialEstimate2.insert(Symbol('x',i+passes*(num_poses-1)), initial_estimate_poses[i]);
   }
 
-   for (int i = 0; i < ground_truth_num_landmarks; i++){
-      initialEstimate2.insert(Symbol('l',i), initial_estimate_landmarks[i]);
-   }
-
-  initialEstimate2.print("Initial estimate: ");
+  //fprintf(stderr, "initial estimate 2\n");
+  //initialEstimate2.print("Initial estimate:");
   //graph.print();
   LevenbergMarquardtOptimizer optimizer(graph, initialEstimate2);
   Values result = optimizer.optimize();
-  result.print("Final: ");
+  //result.print("Final: ");
 
   //Replace initial_estimate with current SLAM values
+  //fprintf(stderr, "optimizer done\n");
   for (int i = 0; i < num_poses; i++){
      initial_estimate_poses[i] = result.at<Pose2>(Symbol('x', i+passes*(num_poses-1)));
   }
-  for (int i = 0; i < ground_truth_num_landmarks; i++){
+  for (int i = 0; i < current_landmark_number; i++){
      initial_estimate_landmarks[i] = result.at<Point2>(Symbol('l', i));
   }
+  //fprintf(stderr, "end\n");
   //Increase the number of passes through the board
   passes++;
 }
@@ -244,8 +255,16 @@ int main(int argc, char** argv) {
     index_to_feature_map[i] = ground_truth_landmarks[i];
   }
 
-   one_pass(graph);
-   one_pass(graph);
-   one_pass(graph);
+   one_pass();
+   ground_truth_landmarks[0] = Vector3(3.1, 2.0, 0.0);
+   index_to_feature_map[3] = index_to_feature_map[2];
+   index_to_feature_map[2] = ground_truth_landmarks[0];
+   current_landmark_number++;
+   debug = true;
+   one_pass();
+   one_pass();
+   one_pass();
+   one_pass();
+   one_pass();
 
 }
